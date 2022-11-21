0a395e1d8684ebcb876fabfb4212b85af2c128a4
Aleksandr Popov <aleneus@gmail.com>
2022-11-21 12:42:50 +0300

    depr: remove testing module and remove pyqtest from requirements

--- appteka/pyqt/testing.py
+++
@@ -1,7 +0,0 @@
-# pylint: skip-file
-# flake8: noqa
-
-from warnings import warn
-warn("module testing is deprecated, use pyqtest", FutureWarning)
-
-from pyqtest import TestApp
--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
     packages=['appteka', 'appteka.pyqt', 'appteka.pyqtgraph'],
     install_requires=[
         'PyQt5>=5.15',
-        'pyqtest>=0.1',
         'pymuplot>=0.1',
     ],
     classifiers=[
